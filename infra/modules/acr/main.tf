82eef636bc50d80f6c8debb7ff1759c3be7f0fb4
ololade <akin0098@algonquinlive.com>
2025-08-08 11:05:55 -0400

    federated identity and weather api key added

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -3,7 +3,7 @@ variable "location" {}
 variable "env" {}
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acr${var.env}"
+  name                = "acrCST8918${var.env}"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
--- infra/modules/redis/main.tf
+++ infra/modules/redis/main.tf
@@ -3,7 +3,7 @@ variable "location" {}
 variable "env" {}
 
 resource "azurerm_redis_cache" "cache" {
-  name                = "redis-${var.env}"
+  name                = "redis-CST8918-${var.env}"
   location            = var.location
   resource_group_name = var.resource_group_name
   capacity            = 1
