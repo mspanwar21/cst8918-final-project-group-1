6925bfc872821c2d2988c63870063c71656c7562
ololade <akin0098@algonquinlive.com>
2025-08-08 08:43:43 -0400

    creation of resources in the azure

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,8 +1,15 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "cst8918-final-project-group-1"
-    storage_account_name = "iacstoragegroup1"
+    storage_account_name = "cst8918finalprojgrp1"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
