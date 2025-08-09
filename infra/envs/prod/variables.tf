16f0de7a0fde076253b8c8c771fe5cc5d3806424
Jonathan Martins <mart1449@algonquinlive.com>
2025-08-08 20:03:55 -0400

    added the workflow automation

--- infra/envs/prod/variables.tf
+++ infra/envs/prod/variables.tf
@@ -1,16 +1,17 @@
 variable "location" {
   default = "canadacentral"
-  type = string
 }
 variable "resource_group_name" {
   default = "cst8918-final-project-group-1"
-  type = string
 }
 variable "env" {
   default = "prod"
-  type = string
 }
 variable "node_count" {
   default = 2
-  type = number
+}
+variable "weather_api_key" {
+  description = "API key for the weather service"
+  type        = string
+  sensitive   = true
 }
